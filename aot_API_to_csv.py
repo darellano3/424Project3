4e28da583f00539f89127b356039b5b724669612
clazca2 <clazca2@uic.edu>
2019-04-07 15:38:47 -0500

    Add files via upload
    Added additional csv files for base data.
    Python script added to make api requests and save as csv

---
+++ aot_API_to_csv.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import requests
+
+class JSON_AOT_Parser:
+    # CONSTANT endpoints for AOT
+    NODES_ENDPOINT = 'https://api.arrayofthings.org/api/nodes'
+    SENSORS_ENDPOINT = 'https://api.arrayofthings.org/api/sensors'
+    OBSERVATIONS_ENDPOINT = 'https://api.arrayofthings.org/api/observations'
+
+    def __init__(self):
+        # nodes dataframe
+        r = requests.get(self.NODES_ENDPOINT)
+        data = r.json()['data']
+        self.nodes = pd.DataFrame(data)
+
+        # sensors dataframe
+        r = requests.get(self.SENSORS_ENDPOINT)
+        data = r.json()['data']
+        self.sensors = pd.DataFrame(data)
+
+        # observations dataframe
+        r = requests.get(self.OBSERVATIONS_ENDPOINT)
+        data = r.json()['data']
+        self.observations = pd.DataFrame(data)
+
+    def processNodes(self):
+        lats = []
+        longs = []
+        for row in self.nodes.itertuples(index=True, name='Pandas'):
+            lats.append(row.location.get('geometry', {}).get('coordinates')[0])
+            longs.append(row.location.get('geometry', {}).get('coordinates')[1])
+        self.nodes['latitude'] = lats
+        self.nodes['longitude'] = longs
+        self.nodes = self.nodes.drop(['location'], axis=1)
+
+    def processSensors(self):
+        pass
+
+    def processObservations(self):
+        pass
+
+def main():
+    parser = JSON_AOT_Parser()
+
+    # write dataframes to csv
+    parser.nodes.to_csv('nodes.csv')
+    parser.sensors.to_csv('sensors.csv')
+    parser.observations.to_csv('observations.csv')
+
+if __name__ == '__main__':
+    main()
