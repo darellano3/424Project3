54b7c3d73eb35c58a6fad8ee66eff0c3d8c4423b
clazca2 <clazca2@uic.edu>
2019-04-29 16:11:30 -0500

    code cleanup - relocated some app.R code to respective files

--- www/Body.R
+++ www/Body.R
@@ -1,12 +1,69 @@
 # This file contains the UI body (visualizations) layout
 
-# Enter your UI elements here
-body<-dashboardBody(
-              div(style = "font-size: 20px; padding: 5px 0px; margin:0%",
-                    column(12,
-                           box(title="Interactive Map of AOT Nodes",solidHeader = TRUE,status="primary",
-                               column(width = 12, leafletOutput(outputId = "Map_nodes"))                               ))
-              )
-                          
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+  
+  #Dashboard Title
+  dashboardHeader(title = "Array of Things Visualization",
+                  titleWidth = 350),
+  sidebar,
+  dashboardBody(
+    
+    shinyDashboardThemes(
+      theme = "purple_gradient"
+    ),
+    
+    #tags$head(tags$style(HTML('
+    # .skin-blue .main-sidebar {
+    #                          background-color: #0072B2;
+    #                          }
+    #                          .skin-blue .sidebar-menu>li.active>a, .skin-blue .sidebar-menu>li:hover>a {
+    #                          background-color: #444444;
+    #                          }
+    #                          '))),
+    
+    tabsetPanel(id = "tabs",
+                
+                tabPanel("Interactive Map", br(), value = "mapTab", 
+                         
+                         div(style = "font-size: 20px; padding: 5px 0px; margin:0%",
+                             
+                             fluidRow(width = 12,
+                                      box( 
+                                        width = 12, title="Graph of AOT Nodes",solidHeader = TRUE,
+                                        plotlyOutput(outputId = "graph", height = 400)
+                                      )
+                             ),
+                             fluidRow(width = 12,
+                                      box( 
+                                        width = 8, title="Interactive Map of AOT Nodes",solidHeader = TRUE,#status="success",
+                                        leafletOutput(outputId = "Map_nodes", height = 800)
+                                      ),
+                                      box(
+                                        width = 4, solidHeader = TRUE, height = 800, title = "Table of Nodes",
+                                        tabsetPanel(
+                                          tabPanel("Temperature",  dataTableOutput("tempTable", height = 800)),
+                                          tabPanel("Humidity", dataTableOutput("humTable", height = 800)),
+                                          tabPanel("Intensity", dataTableOutput("inteTable", height = 800)),
+                                          tabPanel("NO2", dataTableOutput("NO2Table", height = 800)),
+                                          tabPanel("CO", dataTableOutput("COTable", height = 800)),
+                                          tabPanel("SO2", dataTableOutput("SO2Table", height = 800)),
+                                          tabPanel("H2S", dataTableOutput("H2STable", height = 800)),
+                                          tabPanel("OZone", dataTableOutput("OZoneTable", height = 800))
+                                        )
+                                      )
+                             )
+                         ) 
+                ),
+                tabPanel("Heat Map", br(), value = "BTab",
+                         
+                         div(style = "font-size: 20px; padding: 5px 0px; margin:0%",
+                             box(
+                               width = 12, solidHeader = TRUE, leafletOutput("heatMap", height = 1200)
+                             )
+                         )
+                )
+    )#end of tabsetPanel
+  )
 )
 # end dashboard body layout
--- www/Sidebar.R
+++ www/Sidebar.R
@@ -1,13 +1,48 @@
 # This file containts the sidebar for user input 
 
-sidebar <- dashboardSidebar(width=350,
-                            br(),br(),br(),br(),br(),br(),br(),br(),
-                            br(),br(),br(),br(),br(),br(),br(),
+sidebar <- dashboardSidebar(width=350, 
+                            
+                            shiny::conditionalPanel(condition="input$sidebar == 'tabOne'",
+                                                    HTML("
+                                                         <IMG SRC=logo.png ALT='UIC' WIDTH=300 HEIGHT=90>"
+                                                    )
+                                                    ),
+                            
+                            br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),
+                            br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),
+                            
+                            shiny::conditionalPanel(condition="input$sidebar == 'tabOne'",
+                                                    HTML("
+                                                         &nbsp &nbsp <font size=+1> <strong> Project 3: The Air That I Breathe </strong> </font> <br> 
+                                                         &nbsp &nbsp <i> Air quality data in the City of Chicago </i> <br><br>"
+                                                    )
+                                                    ),
                             sidebarMenu(
+                              id ="menu",
                               menuItem('Visualizations',tabName='visualizations',startExpanded = TRUE,
-                                       menuSubItem("Real-Time AQI",tabName='real-time')
+                                       menuSubItem("Interactive Map",tabName='Map'),
+                                       menuSubItem("Heat Map",tabName='B')
                               ),
-                              menuItem('Info',tabName='info')
-                            )
-)
+                              menuItem('Measurement',tabName='Measurement',startExpanded = FALSE,
+                                       menuSubItem("Metric",tabName='Metric'),
+                                       menuSubItem("Imperial",tabName='Imperial')
+                              ),
+                              br(),
+                              shiny::conditionalPanel(condition="input$sidebar == 'tabOne'",
+                                                      HTML("
+                                                           &nbsp &nbsp <b> Authors: </b> <br> 
+                                                           &nbsp &nbsp Daisy Arellano & Jhon Nunez <br> 
+                                                           &nbsp &nbsp Cesar Lazcano & Jay Patel <br> <br>
+                                                           &nbsp &nbsp <b> Libraries Used: </b> <br>
+                                                           &nbsp &nbsp dplyr, ggplot2, ggmap, scales, gridExtra, DT <br>
+                                                           &nbsp &nbsp leaflet, data.table, parallel, readr, shinyWidgets <br> 
+                                                           &nbsp &nbsp lubridate, jpeg, readr, tidyr, plotly <br> <br>
+                                                           &nbsp &nbsp <b> Data: </b> <br>
+                                                           &nbsp &nbsp Data imported from the EPA <br>
+                                                           &nbsp &nbsp (United States Environmental Protection Agency) <br><br><br>
+                                                           "
+                                                      )
+                                                      )
+                                                      )
+                                                      )
 # end sidebar
